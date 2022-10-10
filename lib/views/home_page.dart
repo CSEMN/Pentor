a7a4ce458f43aad0c50965392de27e3d5637d04d
Mahmoud Nasser <csemn.mail@gmail.com>
2022-10-10 20:36:07 +0200

    add: HomePageController
    add HomePageController
    use GetBuilder in Homepage

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,47 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:get/get.dart';
+import 'package:pentor/controllers/HomePageController.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() async {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     final translator = AppLocalizations.of(context)!;
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(translator.homePageTitle),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(translator.homePageCounterText),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+    return GetBuilder<HomePageController>(
+        init: HomePageController(),
+        builder: (controller) => Scaffold(
+              appBar: AppBar(
+                title: Text(translator.homePageTitle),
+              ),
+              body: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(translator.homePageCounterText),
+                    Text(
+                      '${controller.counter}',
+                      style: Theme.of(context).textTheme.headline4,
+                    ),
+                  ],
+                ),
+              ),
+              floatingActionButton: FloatingActionButton(
+                onPressed: controller.onClickAction,
+                tooltip: 'Increment',
+                child: const Icon(Icons.add),
+              ),
+            ));
   }
 }
