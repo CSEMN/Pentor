93815879be62d2741f545982c84988f4c5960b2b
Mahmoud Nasser <csemn.mail@gmail.com>
2022-10-25 21:05:34 +0200

    chor: delete unused imports
    delete imports from old localization library as the project merged to GetX Localization

--- lib/view/dns_test_page.dart
+++ lib/view/dns_test_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:get/get.dart';
 import 'package:pentor/controller/DnsTestController.dart';
 
