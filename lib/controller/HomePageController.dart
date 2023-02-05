5a2fc9140959903a7b75f4d00c1b094007ee62ab
Mahmoud Nasser <csemn.mail@gmail.com>
2023-02-05 20:46:57 +0200

    add WIFI info
    add WIFI info to home page
    add location permission request

--- lib/controller/HomePageController.dart
+++ lib/controller/HomePageController.dart
@@ -1,9 +1,24 @@
 import 'package:get/get.dart';
+import 'package:network_info_plus/network_info_plus.dart';
+import 'package:pentor/model/wifi_info.dart';
+import 'package:permission_handler/permission_handler.dart';
 
-class HomePageController extends GetxController{
-  int counter =0;
-  void onClickAction(){
-    counter++;
-    update();
+class HomePageController extends GetxController {
+  WifiInfo wifiInfo = WifiInfo();
+  final NetworkInfo _networkInfo = NetworkInfo();
+
+  @override
+  void onInit() async {
+    super.onInit();
+    _checkPermissions();
+
   }
+
+  void _checkPermissions() async{
+    if ( await Permission.location.request().isGranted) {
+      wifiInfo = await WifiInfo.initWifiInfo(_networkInfo);
+      update();
+    }
+  }
+
 }
--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:pentor/controller/HomePageController.dart';
+import 'package:expansion_tile_card/expansion_tile_card.dart';
+import 'package:pentor/model/wifi_info.dart';
+import 'package:pentor/themes.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,26 +13,65 @@ class HomePage extends StatelessWidget {
     return GetBuilder<HomePageController>(
         init: HomePageController(),
         builder: (controller) => Scaffold(
-              appBar: AppBar(
-                title: Text('HOME_PAGE_TITLE'.tr),
-              ),
-              body: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text('HOME_PAGE_COUNTER'.tr),
-                    Text(
-                      '${controller.counter}',
-                      style: Theme.of(context).textTheme.headline4,
-                    ),
+            appBar: AppBar(
+              title: Text('HOME_PAGE_TITLE'.tr),
+            ),
+            body: Column(
+              children: [
+                ExpansionTileCard(
+                  expandedTextColor: Themes.primaryColor,
+                  leading: Icon(
+                    Icons.wifi,
+                    size: 50.0,
+                  ),
+                  title: Text(controller.wifiInfo.name!),
+                  subtitle: Text("WIFI Connection"),
+                  children: [
+                    wifiInfoTable(controller.wifiInfo),
                   ],
-                ),
-              ),
-              floatingActionButton: FloatingActionButton(
-                onPressed: controller.onClickAction,
-                tooltip: 'Increment',
-                child: const Icon(Icons.add),
-              ),
-            ));
+                )
+              ],
+            )));
+  }
+
+  Widget wifiInfoTable(WifiInfo wifiInfo) {
+    return DataTable(
+        columns: [
+      DataColumn(
+          label: Text('Field',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold))),
+      DataColumn(
+          label: Text('Value',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold))),
+    ], rows: [
+      DataRow(cells: [
+        DataCell(Text('Name')),
+        DataCell(Text(wifiInfo.name!)),
+      ]),
+      DataRow(cells: [
+        DataCell(Text('IPv4')),
+        DataCell(Text(wifiInfo.ipv4!)),
+      ]),
+      DataRow(cells: [
+        DataCell(Text('Subnet')),
+        DataCell(Text(wifiInfo.subnet!)),
+      ]),
+      DataRow(cells: [
+        DataCell(Text('IPv6')),
+        DataCell(Text(wifiInfo.ipv6!)),
+      ]),
+      DataRow(cells: [
+        DataCell(Text('Gateway')),
+        DataCell(Text(wifiInfo.gateway!)),
+      ]),
+      DataRow(cells: [
+        DataCell(Text('Broadcast')),
+        DataCell(Text(wifiInfo.broadcast!)),
+      ]),
+      DataRow(cells: [
+        DataCell(Text('BSSID')),
+        DataCell(Text(wifiInfo.bssid!)),
+      ]),
+    ]);
   }
 }
