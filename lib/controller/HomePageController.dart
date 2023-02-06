0a71585d85e58ab6cde4aba0ce308805c36f1e03
Mahmoud Nasser <csemn.mail@gmail.com>
2023-02-06 18:45:52 +0200

    add mobile network info
    add mobile network info
    add permission checks
    add network not connected state

--- lib/controller/HomePageController.dart
+++ lib/controller/HomePageController.dart
@@ -1,24 +1,77 @@
+import 'dart:async';
+
 import 'package:get/get.dart';
 import 'package:network_info_plus/network_info_plus.dart';
+import 'package:pentor/model/mobile_network_info.dart';
 import 'package:pentor/model/wifi_info.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 
 class HomePageController extends GetxController {
   WifiInfo wifiInfo = WifiInfo();
   final NetworkInfo _networkInfo = NetworkInfo();
-
+  ConnectivityResult connectivityResult = ConnectivityResult.none;
+  StreamSubscription? _connectivitySubscription;
+  MobileNetworkInfo mobileNetworkInfo = MobileNetworkInfo();
+  bool locationPermissionError=false,phonePermissionError=false;
   @override
   void onInit() async {
     super.onInit();
-    _checkPermissions();
-
-  }
-
-  void _checkPermissions() async{
-    if ( await Permission.location.request().isGranted) {
-      wifiInfo = await WifiInfo.initWifiInfo(_networkInfo);
+    _connectivitySubscription = Connectivity()
+        .onConnectivityChanged
+        .listen((ConnectivityResult result) {
+      connectivityResult = result;
       update();
-    }
+      _checkConnection();
+    });
   }
 
+  @override
+  void dispose() {
+    super.dispose();
+    _connectivitySubscription?.cancel();
+  }
+
+  void _checkConnection() async {
+      switch (connectivityResult) {
+        case ConnectivityResult.wifi:
+          if (await Permission.location.request().isGranted) {
+            wifiInfo = await WifiInfo.initWifiInfo(_networkInfo);
+          }else{
+            this.locationPermissionError=true;
+          }
+          break;
+        case ConnectivityResult.mobile:
+          if (await Permission.phone.request().isGranted) {
+            mobileNetworkInfo= await MobileNetworkInfo.initMobileNetInfo();
+          }else{
+            this.phonePermissionError=true;
+          }
+
+          break;
+      }
+    update();
+  }
+  Future<void> askForLocationPermission() async{
+    var request = await Permission.location.request();
+    if (request.isGranted) {
+      this.locationPermissionError=false;
+      update();
+
+    }else if (request.isPermanentlyDenied){
+      await openAppSettings();
+    }
+    this._checkConnection();
+  }
+
+  Future<void> askForPhonePermission() async{
+    var request = await Permission.phone.request();
+    if (request.isGranted) {
+      this.phonePermissionError=false;
+      update();
+    }else if (request.isPermanentlyDenied){
+      await openAppSettings();
+    }
+    this._checkConnection();
+  }
 }
--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,7 +1,9 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:pentor/controller/HomePageController.dart';
 import 'package:expansion_tile_card/expansion_tile_card.dart';
+import 'package:pentor/model/mobile_network_info.dart';
 import 'package:pentor/model/wifi_info.dart';
 import 'package:pentor/themes.dart';
 
@@ -17,23 +19,82 @@ class HomePage extends StatelessWidget {
               title: Text('HOME_PAGE_TITLE'.tr),
             ),
             body: Column(
-              children: [
-                ExpansionTileCard(
-                  expandedTextColor: Themes.primaryColor,
-                  leading: Icon(
-                    Icons.wifi,
-                    size: 50.0,
-                  ),
-                  title: Text(controller.wifiInfo.name,
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-                  subtitle: Text("WIFI Connection"),
-                  children: [
-                    wifiInfoTable(controller.wifiInfo),
-                  ],
-                )
-              ],
+              children: [connectivityWidget(controller)],
             )));
+  }
+
+  Widget connectivityWidget(HomePageController controller) {
+    if (controller.locationPermissionError || controller.phonePermissionError) {
+      return ListTile(
+        tileColor: Themes.backgroundColor,
+        leading: Icon(
+          Icons.error_outline,
+          color: Colors.red,
+          size: 50.0,
+        ),
+        onTap: () {
+          controller.locationPermissionError
+              ? controller.askForLocationPermission()
+              : controller.askForPhonePermission();
+        },
+        title: Text((controller.locationPermissionError
+            ?'Location':'Phone') +" Permission Not Granted",
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+        subtitle: Text("Tap To Grant"),
+      );
+    } else {
+      switch (controller.connectivityResult) {
+        case ConnectivityResult.none:
+          return ListTile(
+            tileColor: Themes.backgroundColor,
+            leading: Icon(
+              Icons.not_interested,
+              size: 50.0,
+            ),
+            title: Text("NOT CONNECTED",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            subtitle: Text("No Connection Found"),
+          );
+        case ConnectivityResult.wifi:
+          return ExpansionTileCard(
+            expandedTextColor: Themes.primaryColor,
+            leading: Icon(
+              Icons.wifi,
+              size: 50.0,
+            ),
+            title: Text(controller.wifiInfo.name,
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            subtitle: Text("WIFI Network Connection"),
+            children: [
+              wifiInfoTable(controller.wifiInfo),
+            ],
+          );
+        case ConnectivityResult.mobile:
+          return ExpansionTileCard(
+            expandedTextColor: Themes.primaryColor,
+            leading: Icon(
+              Icons.signal_cellular_alt,
+              size: 50.0,
+            ),
+            title: Text(controller.mobileNetworkInfo.name,
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            subtitle: Text("Mobile Network Connection"),
+            children: [mobileNetworkInfoTable(controller.mobileNetworkInfo)],
+          );
+        default:
+          return ListTile(
+            tileColor: Themes.accentColor,
+            leading: Icon(
+              Icons.error_outline,
+              color: Colors.red,
+              size: 50.0,
+            ),
+            title: Text("ERROR",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+            subtitle: Text("CONNECTION ERROR"),
+          );
+      }
+    }
   }
 
   Widget wifiInfoTable(WifiInfo wifiInfo) {
@@ -72,6 +133,30 @@ class HomePage extends StatelessWidget {
       DataRow(cells: [
         DataCell(Text('BSSID')),
         DataCell(Text(wifiInfo.bssid)),
+      ]),
+    ]);
+  }
+
+  Widget mobileNetworkInfoTable(MobileNetworkInfo mobileNetworkInfo) {
+    return DataTable(columns: [
+      DataColumn(
+          label: Text('Field',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold))),
+      DataColumn(
+          label: Text('Value',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold))),
+    ], rows: [
+      DataRow(cells: [
+        DataCell(Text('Name')),
+        DataCell(Text(mobileNetworkInfo.name)),
+      ]),
+      DataRow(cells: [
+        DataCell(Text('Type')),
+        DataCell(Text(mobileNetworkInfo.networkType)),
+      ]),
+      DataRow(cells: [
+        DataCell(Text('Strength')),
+        DataCell(Text(mobileNetworkInfo.signalStrength)),
       ]),
     ]);
   }
