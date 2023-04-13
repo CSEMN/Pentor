f602ccd68262abe9e4dfa776ea426a4ec07203a4
Mahmoud Nasser <mahmoud.nasser.mansour@gmail.com>
2023-04-13 23:58:47 +0200

    fix dns test page
    fix dns test empty page

--- lib/view/dns_test_page.dart
+++ lib/view/dns_test_page.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:pentor/controller/DnsTestController.dart';
+import 'package:pentor/view/widgets/NavigationDrawer.dart';
 
 class DnsTestPage extends StatelessWidget {
   const DnsTestPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      drawer: NavigationDrawerWidget(),
+      appBar: AppBar(
+        title: Text("DNS Test".tr),
+      ),
+    );
   }
 }
