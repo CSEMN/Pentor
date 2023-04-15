1f23311b61c0798adc716e74350b414bfcf3917b
Mahmoud Nasser <mahmoud.nasser.mansour@gmail.com>
2023-04-15 17:55:15 +0200

    add speed test implementation
    implement speed test
    add translation to page

--- lib/view/speed_test_page.dart
+++ lib/view/speed_test_page.dart
@@ -1,17 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:pentor/controller/SpeedTestController.dart';
+import 'package:pentor/themes.dart';
 import 'package:pentor/view/widgets/NavigationDrawer.dart';
+import 'package:speedometer/speedometer.dart';
 
 class SpeedTestPage extends StatelessWidget {
-  const SpeedTestPage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: NavigationDrawerWidget(),
       appBar: AppBar(
         title: Text("Internet Speed Test".tr),
+      ),
+      body: Center(
+        child: GetBuilder<SpeedTestController>(
+          init: SpeedTestController(),
+          builder: (controller) {
+            return Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 35.0,left: 30.0,right: 30.0,bottom: 5.0),
+                  child: SizedBox(
+                    width: 300,
+                    height: 300,
+                    child: SpeedOMeter(
+                        start: controller.start,
+                        end: controller.end,
+                        highlightStart: 0.125,
+                        highlightEnd: 0.875,
+                        themeData: ThemeData(
+                          primaryColor: Themes.primaryColor,
+                          accentColor: Colors.black,
+                          backgroundColor: Colors.grey,
+                        ),
+                        eventObservable: controller.eventObservable),
+                  ),
+                ),
+                Text("Download".tr+" : " + controller.downSpeed.toString() +' '+ controller.downUnit.tr),
+                Text("Upload".tr+" : " + controller.upSpeed.toString() +' '+ controller.upUnit.tr),
+                ElevatedButton(onPressed: controller.speed_test, child: Text("Start".tr))
+              ],
+            );
+          },
+        ),
       ),
     );
   }
