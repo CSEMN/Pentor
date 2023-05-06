53fd71c5dffcaf51399583df5d8b74fb851ee627
AhmedHosni19 <115055717+AhmedHosni19@users.noreply.github.com>
2023-05-06 12:18:06 +0200

    ping_test_page.dart

--- lib/controller/PingTestController.dart
+++ lib/controller/PingTestController.dart
@@ -1,3 +1,29 @@
+import 'package:dart_ping/dart_ping.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
-class PingTestController extends GetxController {}
+class PingTestController extends GetxController {
+  final urlController = TextEditingController();
+  final pingResults = <PingData>[].obs;
+  var transmittedPackets = 0.obs;
+  var lostPackets = 0.obs;
+
+  void doPing() async {
+    final url = urlController.text.trim();
+    final ping = Ping(url, count: 5);
+    final stream = ping.stream;
+
+    pingResults.clear();
+    transmittedPackets.value = 0;
+    lostPackets.value = 0;
+
+    stream.listen((event) {
+      if (event.response?.time != null) {
+        pingResults.add(event);
+      } else {
+        lostPackets.value++;
+      }
+      transmittedPackets.value++;
+    });
+  }
+}
